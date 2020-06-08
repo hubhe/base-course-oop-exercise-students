6b91140c005e3188d0a6231beb933ce2884029a5
Yonatan Lapidus <lapidusy@gmail.com>
2020-06-08 15:38:39 +0300

    Aircraft types fix

--- src/main/java/AerialVehicles/AerialAttackVehicle.java
+++ src/main/java/AerialVehicles/AerialAttackVehicle.java
@@ -1,8 +1,6 @@
 package AerialVehicles;
 
 public interface AerialAttackVehicle{
-    int number_of_missiles =0;
-    String missiles_type = "";
 
     public String attack();
 }
--- src/main/java/AerialVehicles/AerialIntelligenceVehicle.java
+++ src/main/java/AerialVehicles/AerialIntelligenceVehicle.java
@@ -1,7 +1,6 @@
 package AerialVehicles;
 
 public interface AerialIntelligenceVehicle {
-    String sensor_type = "";
 
     public String collectIntelligence();
 }
