c74630fab71c7d49c97fbd22407fdcc0966d1294
Yonatan Lapidus <lapidusy@gmail.com>
2020-06-08 12:14:37 +0300

    Made mission and arial vichile abstract classes

--- src/main/java/AerialVehicles/AerialVehicle.java
+++ src/main/java/AerialVehicles/AerialVehicle.java
@@ -6,4 +6,18 @@ import Missions.MissionTypeException;
 
 
 public abstract class AerialVehicle {
+    String pilot_name;
+    Mission current_mission;
+    int flight_hours_from_last_repair;
+    Boolean is_plain_ready;
+
+    public abstract void flyTo();
+
+    public void check(){
+
+    }
+
+    public void land(){
+        System.out.println("Landing");
+    }
 }
--- src/main/java/Missions/Mission.java
+++ src/main/java/Missions/Mission.java
@@ -1,6 +1,11 @@
 package Missions;
 
-
+import Entities.Coordinates;
 
 public abstract class Mission{
+    Coordinates mission_coordinates;
+
+    public abstract void begin();
+    public abstract void cancel();
+    public abstract void finish();
 }
